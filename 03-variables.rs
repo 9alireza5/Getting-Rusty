5bb665358a56de2dde39a5e990c01a124bb1ff6e
alireza <alireza.vb1995@gmail.com>
2025-03-22 01:04:18 +0330

    Add files via upload

---
+++ 03-variables.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("whats variables");
+    let a:i32=32;
+    let b:i16=15;
+    let c:i63;
+    c=256;
+    let d:char='a';
+    let e:&str="reza1";
+    let f:Strring="reza2";
+    let g:string=string::from("reza3");
+    let h:bool=true;
+    // i still don't know how to print variables :)
+}
