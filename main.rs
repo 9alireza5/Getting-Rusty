22642da2c0970dc7272bae124486adcd97af5f23
alireza <alireza.vb1995@gmail.com>
2025-03-22 17:22:14 +0330

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let a:i16=16;
+    let b:i32=32;
+    let c:i64;
+    c=64;
+
+    let e:char='v';
+
+    let f: &str="reza1";
+    let g: String="reza2".to_owned();
+    let h:String=String::from("reza3");
+
+    let i:bool=true;
+
+    println!("{}",a);
+    println!("{b}");
+    println!("c is equal to {}",c);
+    println!("a is {} and b is {} and c is {}",a,b,c);
+    println!("e is {}",e);
+    println!("f is {} and g is {} and h is {} ",f,g,h);
+    println!("i is {}",i);
+    print!("this is how you ");
+    print!("print without new line");
+
+
+
+
+
+
+}
